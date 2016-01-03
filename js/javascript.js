aa4ba2eb1f139721d683afec16b9d68c41bf3d10
HannibalA <Alawode.Hannibal@gmail.com>
2016-01-03 05:10:30 -0500

    fixed

--- js/javascript.js
+++ js/javascript.js
@@ -5,3 +5,32 @@ $('.datepicker').pickadate({
 $(document).ready(function(){
       $('.parallax').parallax();
     });
+
+var userDate = $("#userDate").val();
+      var bitcoinApiUrl = "http://crossorigin.me/http://api.bitcoincharts.com/v1/markets.json";
+$(document).ready(function(){
+    $(".btn").on("click", function(){
+      
+      
+
+      $.ajax({
+        type: "GET",
+        url: bitcoinApiUrl,
+        success: function(currency) {
+          // for(var i = 0; i < currency.length; i++) {
+            console.log(currency);
+          // }
+        },
+        error:function(jqXHR, textStatus, errorThrown){
+          alert("something went wrong man!")
+        }
+      });
+
+      });
+    });
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
+          
