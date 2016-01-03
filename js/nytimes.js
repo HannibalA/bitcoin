13648cdd2b1a2f4696cabd5538b92a6052941c89
JulioDVega <julio.delavega96@gmail.com>
2016-01-03 04:16:07 -0600

    dunno

--- js/nytimes.js
+++ js/nytimes.js
@@ -1,18 +1,6 @@
 $(document).ready(function(){
+	$("#search").on("click",function(e){
+		var userSearch =
 
-	$("#test").on("click",function(e){
-		e.preventDefault()
-		var nyTimesMovieApiUrl = "http://api.nytimes.com/svc/movies";
-		nyTimesMovieApiUrl += "key=6361b0bb49de7dfe873a73b320c0af73:6:73911657";
-
-		$.ajax({
-			type:"GET"
-			url:nyTimesMovieApiUrl
-				success: function (response){
-					var
-				}
-		})
-
-
-	})
-})
+	});
+});
