45c16ef5afbc1ee24d6c73eeedf2eac804b56606
JulioDVega <julio.delavega96@gmail.com>
2016-01-06 20:03:15 -0600

    commitg to pull

--- js/javascript.js
+++ js/javascript.js
@@ -42,4 +42,12 @@ $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
   });
+
+
+
+
+
+
+
+2
           
